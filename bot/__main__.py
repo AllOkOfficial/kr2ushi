c903e8a8d10725b1261575d9e05e8a761fce1af6
AllOkOfficial <108711998+AllOkOfficial@users.noreply.github.com>
2022-10-15 00:25:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -553,10 +553,10 @@ def main():
                     msg += f"⌚TIME: {time}\n"
                     msg += f"🌐TIMEZONE: {TIMEZONE}\n"
                 else:
-                    msg = f"😎Bot Restarted!\n"
-                    msg += f"📅DATE: {date}\n"
-                    msg += f"⌚TIME: {time}\n"
-                    msg += f"🌐TIMEZONE: {TIMEZONE}"
+                    msg = f"Auto Bot Restarted!\n"
+                    msg += f"DATE: {date}\n"
+                    msg += f"TIME: {time}\n"
+                    msg += f"HOMBALE-TIME: {TIMEZONE}"
 
                 for tag, links in data.items():
                      msg += f"\n{tag}: "
