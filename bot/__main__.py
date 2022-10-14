9ec051320723a405590a0b077fcbcb31077b7fc7
AllOkOfficial <108711998+AllOkOfficial@users.noreply.github.com>
2022-10-15 00:33:46 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -577,7 +577,7 @@ def main():
                     msg = f"Bot Restarted!\n"
                     msg += f"DATE: {date}\n"
                     msg += f"TIME: {time}\n"
-                    msg += f"HOMBALE-TIME: {TIMEZONE}"
+                    msg += f"PLACE: {TIMEZONE}"
 
                 for tag, links in data.items():
                      msg += f"\n{tag}: "
